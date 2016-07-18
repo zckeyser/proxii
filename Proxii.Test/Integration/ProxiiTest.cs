54a047128ef5e476df1fa9815b391b9759963f00
Chris Keyser <ckeyser@emoneyadvisor.com>
2016-07-18 14:45:30 -0400

    added end-to-end tests for MethodNameSelector

--- Proxii.Test/Integration/ProxiiTest.cs
+++ Proxii.Test/Integration/ProxiiTest.cs
@@ -107,5 +107,100 @@ namespace Proxii.Test.Integration
 
 			Assert.Fail("Should have thrown a non-matching exception");
 		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void Proxii_ByMethodName_NoValidNames()
+		{
+			var errors = new List<Exception>();
+
+			Action<Exception> onCatch = e => errors.Add(e);
+
+			var proxy = Proxii.Proxy<IProxiiTester>()
+				.With<ProxiiTester>()
+				.Catch<ArgumentException>(onCatch)
+				.ByMethodName()
+				.Create();
+
+			proxy.Throw(new ArgumentException());
+
+			Assert.Fail("Should have thrown exception.");
+		}
+
+		[TestMethod]
+		public void Proxii_ByMethodName_SingleValidName_Matching()
+		{
+			var errors = new List<Exception>();
+
+			Action<Exception> onCatch = e => errors.Add(e);
+
+			var proxy = Proxii.Proxy<IProxiiTester>()
+				.With<ProxiiTester>()
+				.Catch<ArgumentException>(onCatch)
+				.ByMethodName("Throw")
+				.Create();
+
+			proxy.Throw(new ArgumentException());
+
+			Assert.AreEqual(1, errors.Count);
+			Assert.IsInstanceOfType(errors[0], typeof(ArgumentException));
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void Proxii_ByMethodName_SingleValidName_NonMatching()
+		{
+			var errors = new List<Exception>();
+
+			Action<Exception> onCatch = e => errors.Add(e);
+
+			var proxy = Proxii.Proxy<IProxiiTester>()
+				.With<ProxiiTester>()
+				.Catch<ArgumentException>(onCatch)
+				.ByMethodName("DoActions")
+				.Create();
+
+			proxy.Throw(new ArgumentException());
+
+			Assert.Fail("Should have thrown exception.");
+		}
+
+		[TestMethod]
+		public void Proxii_ByMethodName_MultipleValidNames_Matching()
+		{
+			var errors = new List<Exception>();
+
+			Action<Exception> onCatch = e => errors.Add(e);
+
+			var proxy = Proxii.Proxy<IProxiiTester>()
+				.With<ProxiiTester>()
+				.Catch<ArgumentException>(onCatch)
+				.ByMethodName("DoAction", "Throw")
+				.Create();
+
+			proxy.Throw(new ArgumentException());
+
+			Assert.AreEqual(1, errors.Count);
+			Assert.IsInstanceOfType(errors[0], typeof(ArgumentException));
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void Proxii_ByMethodName_MultipleValidNames_NonMatching()
+		{
+			var errors = new List<Exception>();
+
+			Action<Exception> onCatch = e => errors.Add(e);
+
+			var proxy = Proxii.Proxy<IProxiiTester>()
+				.With<ProxiiTester>()
+				.Catch<ArgumentException>(onCatch)
+				.ByMethodName("DoAction", "DoFunc")
+				.Create();
+
+			proxy.Throw(new ArgumentException());
+
+			Assert.Fail("Should have thrown exception.");
+		}
 	}
 }
