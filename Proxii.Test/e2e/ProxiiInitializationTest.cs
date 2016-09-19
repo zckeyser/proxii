fe14268d4277773e7550489dcd996d09417137a9
zckeyser <zckeyser@gmail.com>
2016-09-19 12:25:41 -0400

    added tests for convenience initializers

--- Proxii.Test/e2e/ProxiiInitializationTest.cs
+++ Proxii.Test/e2e/ProxiiInitializationTest.cs
@@ -33,5 +33,29 @@ namespace Proxii.Test.e2e
 
 			Assert.AreEqual("bar", test[0]);
 		}
-	}
+
+        [TestMethod]
+        public void Proxii_WiresInterfaceToImpl_ByType_ConvenienceMethod()
+        {
+            var proxy = Proxii.Proxy<IProxiiTester>(new ProxiiTester()).Create();
+
+            var test = new[] { "foo" };
+
+            proxy.DoAction(() => test[0] = "bar");
+
+            Assert.AreEqual("bar", test[0]);
+        }
+
+        [TestMethod]
+        public void Proxii_WiresInterfaceToImpl_ByObject_ConvenienceMethod()
+        {
+            var proxy = Proxii.Proxy<IProxiiTester, ProxiiTester>().Create();
+
+            var test = new[] { "foo" };
+
+            proxy.DoAction(() => test[0] = "bar");
+
+            Assert.AreEqual("bar", test[0]);
+        }
+    }
 }
